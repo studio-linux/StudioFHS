06f88593259326d78b215ebbb4b0375133b9af9f
Alexander Marchenko <bourne-sci-hack@yandex.ru>
2020-08-17 14:27:08 +1000

    Create api.lisp

---
+++ src/api.lisp
@@ -0,0 +1,87 @@
+(in-package :api)
+
+
+
+
+
+(declaim (symbol *ui-mode*
+		 *language*)
+	 (string *error-command-message*
+		 *error-search-message*
+		 *error-set-language-message*))
+
+(defparameter *ui-mode*  'text-mode)
+(defparameter *language* 'en)
+
+(defparameter *error-command-message*       "Error input: command not found :(")
+(defparameter *error-search-message*        "Error input: no such file or directory :(")
+(defparameter *error-set-language-message*  "Error input: invalid number set :(")
+
+
+
+
+(defun path-include-list-p (object)
+  (and (consp object)
+       (consp (nth 1 object))
+       (consp (nth 3 object))
+       (equal (nth 0 object) :directories)
+       (equal (nth 2 object) :files)
+       (every #'stringp (nth 1 object))
+       (every #'stringp (nth 3 object))))
+
+
+(deftype path-include-list ()
+  `(satisfies path-include-list-p))
+
+
+
+
+(defstruct (path (:constructor  add-path)
+		 (:print-object print-path))
+
+  (type            ""  :type string               :read-only false)
+  (path            ""  :type string               :read-only false)
+  (name            ""  :type string               :read-only false)
+  (description-ru  ""  :type string               :read-only false)
+  (description-en  ""  :type string               :read-only false)
+  (include         ""  :type path-include-list    :read-only false))
+
+
+(defun print-path (path stream)
+  (cond ((equal *ui-mode* 'text-mode) (print-path-text path stream))))
+
+
+
+
+(defmacro check-path (str)
+  `(let* ((var  (read-from-string (concatenate 'string "*" ,str "*")))
+	  (path (ignore-errors (eval var))))
+     (cond ((path-p path) (format t "~%~A~2%" path))
+	   (t (format t " * ~A~%" *error-search-message*)))))
+
+
+
+
+(defun main ()
+  (tagbody
+
+   :start
+     (case *ui-mode*
+       (text-mode    (boot))
+       (ncurses-mode (print "Development me..."))
+       (gui-mode     (print "Development me..."))
+       (otherwise
+	(format t "Error in argument: \"~A\"~%" *ui-mode*)
+	(format t "Please, use only \"text-mode\", \"ncurses-mode\" or \"gui-mode\"")))
+
+   :shell
+     (format t " [Studio FHS]: ")
+     (let* ((in-command (ignore-errors (read *standard-input*)))
+	    (in-search (string in-command)))
+       (case in-command
+	 (help     (print-help))
+	 (language (set-language))
+	 (quit     (abort))
+	 (otherwise (cond ((equal #\/ (char in-search 0)) (check-path in-search))
+			  ((format t " * ~A~%" *error-command-message*)))))
+       (go :shell))))
